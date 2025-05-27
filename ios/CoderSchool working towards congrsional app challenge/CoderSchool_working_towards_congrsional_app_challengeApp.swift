1c2892e397f722ae1812419c2d35a0168a3b8e38
Shriyap19 <shriyapatle36@gmail.com>
2025-05-27 17:55:00 -0400

    Upload ios project folder

---
+++ ios/CoderSchool working towards congrsional app challenge/CoderSchool_working_towards_congrsional_app_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoderSchool_working_towards_congrsional_app_challengeApp.swift
+//  CoderSchool working towards congrsional app challenge
+//
+//  Created by Shriya Patel on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct CoderSchool_working_towards_congrsional_app_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/CoderSchool working towards congrsional app challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoderSchool working towards congrsional app challenge
+//
+//  Created by Shriya Patel on 5/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

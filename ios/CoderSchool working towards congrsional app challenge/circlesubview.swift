41cb6fd00fd1c8b32bb244efc8673d61cd5b610d
Shriyap19 <shriyapatle36@gmail.com>
2025-06-10 17:58:40 -0400

    Creating the grid of circles representing a month

---
+++ ios/CoderSchool working towards congrsional app challenge/circlesubview.swift
@@ -0,0 +1,18 @@
+//
+//  circlesubview.swift
+//  CoderSchool working towards congrsional app challenge
+//
+//  Created by Shriya Patel on 6/3/25.
+//
+
+import SwiftUI
+
+struct circlesubview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    circlesubview()
+}

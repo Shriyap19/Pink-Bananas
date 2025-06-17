82dc98fc70557cd20281365f3d2a545a27538b16
Shriyap19 <shriyapatle36@gmail.com>
2025-06-17 17:37:34 -0400

    A list of circles, base for calender on homepage(will edit later)

--- ios/CoderSchool working towards congrsional app challenge/circlesubview.swift
+++ ios/CoderSchool working towards congrsional app challenge/circlesubview.swift
@@ -7,9 +7,48 @@
 
 import SwiftUI
 
+
 struct circlesubview: View {
+    @State private var symbolname = "largecircle.fill.circle"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Home Page").padding(.leading).font(.custom("Furtura", size: 40)).frame(alignment: .top)
+            //Can't being Home PAge text to the top
+            
+            Spacer()
+        }
+           
+            
+       
+          
+            
+            
+        VStack {
+            Grid(horizontalSpacing: 10, verticalSpacing: 10) {
+                ForEach(1...4, id: \.self) { y in GridRow {
+                    ForEach(1...7, id: \.self) { x in
+                        VStack(spacing: 0){
+                            Image(systemName:symbolname ).font(.largeTitle).foregroundColor(.gray)
+                                .frame(maxWidth: 60, maxHeight: 100)
+                                .aspectRatio(1, contentMode: .fit)
+                            Text(String(y*x)).font(.callout).foregroundColor(.gray)
+                        }
+                       
+                        
+                    }
+                }
+                    
+                }
+                
+            }
+            Spacer().frame(height:290)
+
+        }
+            
+        
+       
+        
+
     }
 }
 

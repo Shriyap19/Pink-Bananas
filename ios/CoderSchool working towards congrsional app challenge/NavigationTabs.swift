311f57c0b86208903137af329753789c0e6d6fad
Shriyap19 <shriyapatle36@gmail.com>
2025-09-02 17:47:18 -0400

    Added mainappview to naviagation tabs

--- ios/CoderSchool working towards congrsional app challenge/NavigationTabs.swift
+++ ios/CoderSchool working towards congrsional app challenge/NavigationTabs.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct NavigationTabs: View {
     var body: some View {
         TabView {
-            Image(systemName: "star").tabItem { Image(systemName: "house")
+            MainAppView().tabItem { Image(systemName: "house")
                 Text("Home")
         }
             Image(systemName: "star").tabItem { Image(systemName: "star")

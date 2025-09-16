8247c398eb88d13ad732cdee4d21a022458bb559
Shriyap19 <shriyapatle36@gmail.com>
2025-09-16 17:24:22 -0400

    Making restircted apps selection able to be stored and shared throughout the app

--- ios/CoderSchool working towards congrsional app challenge/CoderSchool_working_towards_congrsional_app_challengeApp.swift
+++ ios/CoderSchool working towards congrsional app challenge/CoderSchool_working_towards_congrsional_app_challengeApp.swift
@@ -13,8 +13,10 @@ import FamilyControls
 @main
 
 struct CoderSchool_working_towards_congrsional_app_challengeApp: App {
+    @StateObject private var familyManager = FamilyControlsManager() //so varible is for app, not just onboarding
     var body: some Scene {
         WindowGroup {
-            ContentView()}
+            ContentView().environmentObject(familyManager) //  makes it global
+        }
     }
 }

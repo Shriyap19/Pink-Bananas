980dc500361daf67e3ded76ecb73ea0c8c3fefcf
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-10-21 12:26:49 -0400

    Create new views for add a new restricted app

---
+++ ios/CoderSchool working towards congrsional app challenge/RestrictedAppsView.swift
@@ -0,0 +1,18 @@
+//
+//  RestrictedAppsView.swift
+//  CoderSchool working towards congrsional app challenge
+//
+//  Created by Jared Sinai Hernandez Adame on 10/21/25.
+//
+
+import SwiftUI
+
+struct RestrictedAppsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestrictedAppsView()
+}
---
+++ ios/CoderSchool working towards congrsional app challenge/ValidateRestrictedAppView.swift
@@ -0,0 +1,18 @@
+//
+//  ValidateRestrictedAppView.swift
+//  CoderSchool working towards congrsional app challenge
+//
+//  Created by Jared Sinai Hernandez Adame on 10/21/25.
+//
+
+import SwiftUI
+
+struct ValidateRestrictedAppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ValidateRestrictedAppView()
+}

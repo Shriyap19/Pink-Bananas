016e9314121f4b3491c4ecf960b8f3a0ed1a84c7
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-10-24 17:11:00 -0400

    Test code for the device activity monitor

--- ios/DAM/DeviceActivityMonitorExtension.swift
+++ ios/DAM/DeviceActivityMonitorExtension.swift
@@ -6,43 +6,65 @@
 //
 
 import DeviceActivity
+import os.log
+import Foundation
+
+
+let logger = OSLog(subsystem: "com.tcsm.orangeteamproject", category: "DeviceActivityMonitor")
 
 // Optionally override any of the functions below.
 // Make sure that your class name matches the NSExtensionPrincipalClass in your Info.plist.
 class DeviceActivityMonitorExtension: DeviceActivityMonitor {
+    
     override func intervalDidStart(for activity: DeviceActivityName) {
         super.intervalDidStart(for: activity)
         
         // Handle the start of the interval.
+        os_log("interval started 2", log:logger)
+        
+        UserDefaults(suiteName: "group.com.tcsm.orangeteamproject")?.set("Interval started", forKey:"shouldShowAlert")
+        
     }
     
     override func intervalDidEnd(for activity: DeviceActivityName) {
         super.intervalDidEnd(for: activity)
         
         // Handle the end of the interval.
+        // Handle the start of the interval.
+        os_log("interval ended", log:logger)
+        UserDefaults(suiteName: "group.com.tcsm.orangeteamproject")?.set("Interval ended", forKey:"shouldShowAlert")
     }
     
     override func eventDidReachThreshold(_ event: DeviceActivityEvent.Name, activity: DeviceActivityName) {
         super.eventDidReachThreshold(event, activity: activity)
         
+        UserDefaults(suiteName: "group.com.tcsm.orangeteamproject")?.set("Threshold reached!",forKey:"shouldShowAlert")
         // Handle the event reaching its threshold.
+        // Handle the start of the interval.
+        os_log("event reached its threshold", log:logger)
     }
     
     override func intervalWillStartWarning(for activity: DeviceActivityName) {
         super.intervalWillStartWarning(for: activity)
         
         // Handle the warning before the interval starts.
+        // Handle the start of the interval.
+        os_log("interval will start warning", log:logger)
     }
     
     override func intervalWillEndWarning(for activity: DeviceActivityName) {
         super.intervalWillEndWarning(for: activity)
         
         // Handle the warning before the interval ends.
+        // Handle the start of the interval.
+        os_log("interval will end warning", log:logger)
     }
     
     override func eventWillReachThresholdWarning(_ event: DeviceActivityEvent.Name, activity: DeviceActivityName) {
         super.eventWillReachThresholdWarning(event, activity: activity)
         
+        UserDefaults(suiteName: "group.com.tcsm.orangeteamproject")?.set("event will reach threshold",forKey:"shouldShowAlert")
         // Handle the warning before the event reaches its threshold.
+        os_log("event will reach threshold", log:logger)
     }
 }
